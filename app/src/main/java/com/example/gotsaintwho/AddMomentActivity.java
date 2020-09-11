5ed4985719ca76b025a75432d7e370e63a25b443
dfei <dfei@student.unimelb.edu.au>
2020-09-11 11:09:24 +1000

    small change

--- app/src/main/java/com/example/gotsaintwho/AddMomentActivity.java
+++ app/src/main/java/com/example/gotsaintwho/AddMomentActivity.java
@@ -58,7 +58,6 @@ public class AddMomentActivity extends AppCompatActivity {
     String imageId;
 
     private User user;
-    String momentEditTextStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
