536dc684eb19a76efe17b934c8b5b834ba18bd03
Olivia0012 <lu.wang4@student.unimelb.edu.au>
2020-11-05 15:42:11 +1100

    Merge

--- app/src/main/java/com/example/gotsaintwho/fragment/MultifunctionFragment.java
+++ app/src/main/java/com/example/gotsaintwho/fragment/MultifunctionFragment.java
@@ -1,14 +1,19 @@
 package com.example.gotsaintwho.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.gotsaintwho.MomentActivity;
+import com.example.gotsaintwho.R;
 import com.example.gotsaintwho.TrackActivity;
 import com.example.gotsaintwho.VoiceMemoActivity;
 import com.example.gotsaintwho.adapter.MultifunctionAdapter;
@@ -46,7 +51,7 @@ public class MultifunctionFragment extends Fragment {
         return view;
     }
 
-    private void initMultifunction(){
+    private void initMultifunction() {
         //第一个是朋友圈
         multifunctionList.add(new Multifunction("Moment", R.drawable.plane, new ViewOnClickListener() {
             @Override
@@ -62,7 +67,11 @@ public class MultifunctionFragment extends Fragment {
             @Override
             public void execute() {
                 Intent intent = new Intent(getActivity(), TrackActivity.class);
-        //th
+                startActivity(intent);
+
+            }
+        }));
+        
         multifunctionList.add(new Multifunction("Voice Memos", R.drawable.voice_memos, new ViewOnClickListener() {
             @Override
             public void execute() {
@@ -72,4 +81,5 @@ public class MultifunctionFragment extends Fragment {
             }
         }));
     }
+
 }
